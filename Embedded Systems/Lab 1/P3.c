a07e94b692a838505c8b6a2fec07c4e48e0c2099
Jaiden Green <143838602+JaidenTGreen@users.noreply.github.com>
2025-02-01 19:55:51 -0600

    Update and rename Q3.c to P3.c

--- Embedded Systems/Lab 1/P3.c
+++ Embedded Systems/Lab 1/P3.c
@@ -1,48 +1,41 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
-//mostly works
+//works
 bool check(int i){
-    if (i <= 1){
+    if (i < 2){
         return false;
-    } 
-
-    for(int j = 2; j <= sqrt(i); j++){
+    }
+    for(int j = 2; j * j <= i; j++){
         if (i % j == 0){
             return false;
         }
-    return true;
     }
+    return true;
 }
 
 int main()
 {
     int input;
-    int out[] = {};
+
 
     printf("Enter a number: \n");
     scanf("%d", &input);
 
+    int out[input]; //stores memory slots up to the input
     int y = 0;
 
     for (int x = 1; x <= input; x++){
-        //printf("%d\n", x);
+        //printf("test: %d\n", x);
         if (check(x)){
             out[y] = x;
-            printf("%d\n", out[y]);
+            //printf("%d\n", out[y]); test case
             y++;
         }
     }
+    for (int k = 0; k < y; k++)
+    {
+        printf("%d ", out[k]);
+    }
     
-
-
-
-    if (check(input)){
-        printf("Is a prime");
-    }
-    else{
-        printf("Not a prime");
-    }
-
-
 }
